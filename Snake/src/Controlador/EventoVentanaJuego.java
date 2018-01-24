af11a8b9bd3193371a9a77d72df0ed43edba0e64
Estudiante <Estudiante@lab0515>
2018-01-24 13:46:55 -0500

    correccion de ventanas

--- Snake/src/Controlador/EventoVentanaJuego.java
+++ Snake/src/Controlador/EventoVentanaJuego.java
@@ -15,14 +15,14 @@ import java.awt.event.ActionListener;
  * @author Estudiante
  */
 public class EventoVentanaJuego implements ActionListener {
-    
+
     private VentanaJuego vJuego;
     private VentanaPrincipal vPrincipal;
 
     public EventoVentanaJuego(VentanaJuego vJuego) {
         this.vJuego = vJuego;
         this.vPrincipal = vPrincipal;
-    }    
+    }
 
     public VentanaJuego getvJuego() {
         return vJuego;
@@ -39,27 +39,28 @@ public class EventoVentanaJuego implements ActionListener {
     public void setvPrincipal(VentanaPrincipal vPrincipal) {
         this.vPrincipal = vPrincipal;
     }
-   
 
     @Override
     public void actionPerformed(ActionEvent ae) {
-        
-        if(ae.getSource().equals(this.vJuego.getMenuItemList().get(0))){
+
+        if (ae.getSource().equals(this.vJuego.getMenuItemList().get(0))) {
             System.out.println("hola");
-            this.vJuego = new VentanaJuego();
-            
-            
+            VentanaJuego v = new VentanaJuego();
+            v.setTitle("Snake");
+            v.setSize(486, 508);
+            v.setLocationRelativeTo(null);
+            v.setResizable(false);
+            v.setVisible(true);
+
         }
-        
-        if(ae.getSource().equals(this.vJuego.getMenuItemList().get(1))){
+
+        if (ae.getSource().equals(this.vJuego.getMenuItemList().get(1))) {
             this.vJuego.setVisible(false);
             this.vJuego.getJuego().stop();
             this.vPrincipal = new VentanaPrincipal();
             this.vPrincipal.setVisible(true);
-            
-            
+
         }
     }
-    
-    
+
 }
